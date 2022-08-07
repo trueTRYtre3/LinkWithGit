32db816e19c6c34751d1a2eac29f89017d58cb1e
Kyle Ortega <kyleortega253@gmail.com>
2022-08-07 16:57:12 -0700

    more trials

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(sayHello());
+    }
+
+    private static String sayHello() {
+        return "Hello World, this is overkill!";
     }
 }
